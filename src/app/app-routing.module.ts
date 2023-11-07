bf76944d9405a8afb4d08916264dbcd5cb0e3d77
jroserog <jroserog@itsqmet.edu.ec>
2023-11-07 10:09:42 -0500

    creación de ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
 import { InfoComponent } from './screens/info/info.component';
+import { ProfeComponent } from './components/profe/profe.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'info', component: InfoComponent}
+  { path: 'info', component: InfoComponent},
+  { path: 'profe', component: ProfeComponent}
 ];
 
 @NgModule({
