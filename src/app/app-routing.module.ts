1a4d281e808cfd56d319da22b9308b59a9bbe3a8
EliAlarcon <EliAlarcon@users.noreply.github.com>
2023-11-07 10:17:07 -0500

    Ruta componente elizabeth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
 import { InfoComponent } from './screens/info/info.component';
+import { ElizabethComponent } from './components/elizabeth/elizabeth.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'info', component: InfoComponent}
+  { path: '', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'elizabeth', component: ElizabethComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
